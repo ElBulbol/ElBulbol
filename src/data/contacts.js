707e2fc00a0a4feb3afd79e0e9f488d89c3bf885
ElBulbol <belalmoh200617@gmail.com>
2026-09-10 00:50:05 +0300

    idk

---
+++ src/data/contacts.js
@@ -0,0 +1,27 @@
+export const contacts = [
+  {
+    label: 'email',
+    value: 'belal.alshrif@gmail.com',
+    url: 'mailto:belal.alshrif@gmail.com',
+  },
+  {
+    label: 'github',
+    value: 'ElBulbol',
+    url: 'https://github.com/ElBulbol',
+  },
+  {
+    label: 'linkedin',
+    value: 'belal-alshref',
+    url: 'https://linkedin.com/in/belal-alshref-09b1b9323',
+  },
+  {
+    label: 'tryhackme',
+    value: 'ElBulbol',
+    url: 'https://tryhackme.com/p/ElBulbol',
+  },
+  {
+    label: 'discord',
+    value: 'open profile',
+    url: 'https://discord.com/users/1090613731601436724',
+  },
+];
